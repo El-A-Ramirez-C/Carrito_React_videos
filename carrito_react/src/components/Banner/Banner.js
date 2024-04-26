0ad818bbd8afdeb34c406526b94e37faa4072034
El_A_C <161752312+El-A-Ramirez-C@users.noreply.github.com>
2024-04-26 11:00:14 -0600

    Add files via upload

---
+++ carrito_react/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container"></div>
+        <h1>Books Shop</h1>
+        <p>The Best Place To Find Book</p>
+    </div>
+  );
+}
+export default Banner;
